d63544de5f69e739026b027ea02f763b117c071e
0maru <0maru.dev@gmail.com>
2020-11-30 23:50:39 +0900

    add rewardedAd show method

---
+++ ios/Classes/RewardedAdController.swift
@@ -0,0 +1,8 @@
+//
+//  RewardedAdController.swift
+//  maio_flutter
+//
+//  Created by 0maru on 2020/11/30.
+//
+
+import Foundation
---
+++ ios/Classes/ViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController.swift
+//  maio_flutter
+//
+//  Created by 0maru on 2020/11/30.
+//
+
+import Foundation
