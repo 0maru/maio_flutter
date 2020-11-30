659cb857f89bde4b5d9268eb7c6736aab345f57e
0maru <0maru.dev@gmail.com>
2020-11-30 23:52:17 +0900

    add export rewareded

--- lib/maio_flutter.dart
+++ lib/maio_flutter.dart
@@ -1,14 +1,1 @@
-
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class MaioFlutter {
-  static const MethodChannel _channel =
-      const MethodChannel('maio_flutter');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'rewardedad.dart';
